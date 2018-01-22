4fe1cc1d0886c588e11b8fb8606214be7967498b
Sabah Nawar <sabah.nawar@gmail.com>
2018-01-22 17:21:49 -0500

    figuring out nav

--- build/highlight.js
+++ build/highlight.js
@@ -1,24 +1,31 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+      
 
-
-// Fixed Nav
-jQuery(document).ready(function ($) {
-    $(window).scroll(function(){
-        var scrollTop = 142;
-        if($(window).scrollTop() >= scrollTop){
-            $('nav').css({
-                position : 'fixed',
-                top : '0'
-            });
-        }
-        if($(window).scrollTop() < scrollTop){
-            $('nav').removeAttr('style');   
-        }
-    })
-  
-  // Active Nav Link
-  $('nav ul li a').click(function(){
-         $('nav ul li a').removeClass('active');
-         $(this).addClass('active');
     });
-})
+});
 
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#nav-menu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos-90) {
+            $('#nav-menu ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
